2502c3813bd089afb5b3531cb8af75bd6ad86dec
Asura14 <g.luismiguel14@gmail.com>
2015-05-19 16:39:43 +0100

    Completed ChicaGO and added JSON

--- Entry.java
+++ Entry.java
@@ -2,6 +2,7 @@ import java.util.*;
 import Exceptions.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.text.DateFormatSymbols;
 
 public class Entry {
 
@@ -28,6 +29,7 @@ public class Entry {
 	}
 
 	private String cleanProperty(String property) {
+		property = property.trim();
 		if(property.charAt(0) == '{') {
 			property = property.substring(1, property.length() - 1);
 		}
@@ -39,7 +41,11 @@ public class Entry {
 	{
 		if(property.equals("author")) {
 			//TODO: transform author into right format
-		}
+		} else if (property.equals("month")) {
+			content = getMonth(Integer.parseInt(content));
+		} else if (property.equals("pages")) {
+			content = content.replace("--", "&mdash;");
+		} 
 
 		Pattern accute = Pattern.compile("\\\\'\\{([a-zA-Z])\\}");
 		Matcher match = accute.matcher(content);
@@ -74,5 +80,11 @@ public class Entry {
 
 		return template;
 	}
+
+	public String getMonth(int month) {
+    	return new DateFormatSymbols().getMonths()[month-1];
+    }
+
+
 
 }
